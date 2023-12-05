33da182fed0079bd76f8aff9a3a913fbd5086713
mcarri355 <carrillomatisa@gmail.com>
2023-12-05 15:16:42 -0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,8 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState(false);
-  const [tours, setTours] = useState([]);
-
-  useEffect(() => {
-    fetch('https://course-api.com/react-tours-project')
-      .then((response) => {
-        if (response.status >= 200 && response.status <= 299) {
-          return response.json();
-        } else {
-          setIsLoading(false);
-          setIsError(true);
-          throw new Error(response.statusText);
-        }
-      })
-      .then((data) => {
-        setTours(data);
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        console.log(error);
-        setIsError(true);
-      });
-  }, []);
   
-  return (
-    <div className="App">
-      <header className="App-header">
-        {}
-      </header>
-    </div>
-  );
 }
 
 export default App;
