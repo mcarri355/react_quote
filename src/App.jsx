510ec92f0dd599a28ae77c09c26aa2d739ebb1d7
mcarri355 <carrillomatisa@gmail.com>
2023-12-06 19:21:12 -0700

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,74 @@
-import { useState, useEffect } from 'react';
+// App.js
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  
+  const [quoteData, setQuoteData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    fetch('https://api.quotable.io/random')
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          setIsLoading(false);
+          setIsError(true);
+          throw new Error(response.statusText);
+        }
+      })
+      .then((data) => {
+        setQuoteData(data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setIsError(true);
+      });
+  }, []);
+
+  const handleRefresh = () => {
+    setIsLoading(true);
+    setIsError(false);
+    fetch('https://api.quotable.io/random')
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          setIsLoading(false);
+          setIsError(true);
+          throw new Error(response.statusText);
+        }
+      })
+      .then((data) => {
+        setQuoteData(data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setIsError(true);
+      });
+  };
+
+  if (isLoading) {
+    return <div className="container"><h1>Loading....</h1></div>;
+  }
+
+  if (isError) {
+    return <div className="container"><h1>Error....</h1></div>;
+  }
+
+  return (
+    <div className="container">
+      <h1>Random Quote</h1>
+      <blockquote>
+        <p>{quoteData.content}</p>
+        <footer>{quoteData.author}</footer>
+      </blockquote>
+      <button onClick={handleRefresh}>Refresh</button>
+    </div>
+  );
 }
 
 export default App;
